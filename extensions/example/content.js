4454f55f5c8c7729f046354a48c7103d02f83f3d
Arthur Kim <akim15@risd.edu>
2018-04-11 19:40:24 -0400

    push

--- extensions/example/content.js
+++ extensions/example/content.js
@@ -1,26 +1,32 @@
 // replace images when the page loads
-$('img').attr('src', 'https://i.imgur.com/8QMMdNv.jpg');
+
+// $('img').attr('src', 'https://i.imgur.com/8QMMdNv.jpg');
 
 // replace text in the page when it loads
-$('body').children().each(function () {
-	// replace the '@' sign with a '$' sign
-	$(this).html( $(this).html().replace(/@/g,'$') );
-});
+
+// $('body').children().each(function () {
+// 	// replace the '@' sign with a '$' sign
+// 	$(this).html( $(this).html().replace(/@/g,'$') );
+// });
 
 // when you click on an image hide it using CSS
-$('img').click(function() {
-	$(this).addClass('hide');
-});
+
+// $('img').click(function() {
+// 	$(this).addClass('hide');
+// });
 
 // when you click on the page 'body' apply .gradient to it from the CSS
-$('body').click(function() {
-	$(this).addClass('gradient');
-});
 
-$('div').click(function() {
-	$(this).fadeOut();
-});
+// $('body').click(function() {
+// 	$(this).addClass('gradient');
+// });
 
+// $('div').click(function() {
+// 	$(this).fadeOut();
+// });
 
+$('img').click(function(){
+ $(this).addClass('dog')
+})
 
 
