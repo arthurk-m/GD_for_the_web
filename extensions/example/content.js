9270828071cd964076186dd37cdb22a0dbad025a
Arthur Kim <akim15@risd.edu>
2018-04-19 22:51:10 -0400

    commit

--- extensions/example/content.js
+++ extensions/example/content.js
@@ -1,12 +1,10 @@
 // replace images when the page loads
 
-// $('img').attr('src', 'https://i.imgur.com/8QMMdNv.jpg');
-
 // replace text in the page when it loads
 
 // $('body').children().each(function () {
 // 	// replace the '@' sign with a '$' sign
-// 	$(this).html( $(this).html().replace(/@/g,'$') );
+// 	$(this).html( $(this).html().replace(a b c d e f g h i j k l m n o p q r s t u v w x y z,'spin') );
 // });
 
 // when you click on an image hide it using CSS
@@ -21,12 +19,39 @@
 // 	$(this).addClass('gradient');
 // });
 
+$('img').click(function() {
+    $(this).addClass('rotate');
+})
+
+// Facebook
+
+//$('img').attr('src', 'https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('div').attr('src', 'https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('body').attr('src', 'https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('#pinnedNav.homeSideNav').attr('src', 'https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('#appsNav').attr('src','https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('#createNav').attr('src','https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('.homeSideNav').attr('src','https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+// $('.linkWrap.noCounts').attr('src','https://cdn.abcotvs.com/dip/images/3324594_041018-kgo-ap-mark-zuckerberg-congress-testify-img.jpg');
+
+$('div').click(function() {
+    $(this).addClass('rotate');
+});
+
+// $(body).click(function() {
+// 	$(this).addClass('.transparent')
+// });
+
+// $(window).scroll(function(){
+	
+// })
+
 // $('div').click(function() {
 // 	$(this).fadeOut();
 // });
 
-$('img').click(function(){
- $(this).addClass('dog')
-})
+// $('img').click(function() {
+//  $(this).addClass('dog');
+// });
 
 
